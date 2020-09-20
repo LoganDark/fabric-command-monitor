ae59e4e066b36aee34a3c1298159d2f00941605d
Logan Darklock <logandarklock@gmail.com>
2020-09-20 14:11:28 -0700

    Remove hello world print...

--- src/main/kotlin/net/logandark/commandmonitor/CommandMonitor.kt
+++ src/main/kotlin/net/logandark/commandmonitor/CommandMonitor.kt
@@ -25,12 +25,6 @@ object CommandMonitor : ModInitializer {
 	lateinit var server: MinecraftServer
 
 	override fun onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		println("Hello Fabric world!")
-
 		// Cycle config to ensure the latest values are read & any errors are
 		// fixed and written to file.
 		CommandMonitorConfig.load()
